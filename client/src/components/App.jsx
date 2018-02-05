ac341ac2db891f861ecd671b5fbe3a3778995986
Max Tam <max.c.tam@gmail.com>
2018-02-04 17:20:56 -0800

    aws mt4 deployment ready

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
+/*eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Component1 from './Component1.jsx';
-import Component2 from './Component2.jsx';
 
 /**
  * Description:
@@ -15,51 +14,172 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      previousView: [],
+      currentView: ['homepage'],
+      forwardView: [],
     };
   }
 
-  //Fires before render()
-  componentWillMount() {
+  pageBack() {
+    let { previousView, currentView, forwardView } = this.state;
+
+    if(previousView.length > 0) {
+      forwardView.push(currentView.pop());
+      currentView.push(previousView.pop());
+
+      this.setState({
+        previousView,
+        currentView,
+        forwardView,
+      });
+    }
   }
 
-  //Fires after render()
-  componentDidMount() {
+  pageForward() {
+    let { previousView, currentView, forwardView } = this.state;
+
+    if(forwardView.length > 0) {
+      previousView.push(currentView.pop());
+      currentView.push(forwardView.pop());
+
+      this.setState({
+        previousView,
+        currentView,
+        forwardView,
+      });
+    }
   }
 
-  //Test Function
-  fn() {
-    console.log('fn fired');
+  changeView(view) {
+    let { previousView, currentView, forwardView } = this.state;
+    forwardView = [];
+    previousView.push(currentView.pop());
+    currentView.push(view);
+
     this.setState({
-      test: 'setting state',
+      previousView,
+      currentView,
+      forwardView,
     });
   }
 
-  //Notes on Render Function:
-  /* Make sure return has its opening parenthesis on the same line. */
+  renderView(view) {
+    if (view === 'homepage') {
+      return (<HomePage changeView={this.changeView.bind(this)}/>);
+    } else if (view === 'imagehost') {
+      return (<ImageHost changeView={this.changeView.bind(this)}/> );
+    } else if (view === 'packuno') {
+      return (<Packuno changeView={this.changeView.bind(this)}/> );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>HELLO</h1>
-        <Component1
-          state={this.state}
-          fn={this.fn.bind(this)}
-        />
-        <Component2
-          list = {['1','2','3']}
-          doThis = {()=>10}
-          hashTable = {{obj:1}}
-        />
+      <div className="material" >
+        <nav className="navbar navbar-light bg-light">
+          <div className="btn-group" role="group" aria-label="Basic example" style={{}}>
+            <button type="button" className="btn btn-secondary btn-lg"
+              style={{marginLeft: '3vw', marginRight: '40px'}}
+              onClick={this.pageBack.bind(this)} >&lt;
+            </button>
+            <button type="button" className="btn btn-secondary btn-lg"
+              onClick={this.pageForward.bind(this)}>&gt;
+            </button>
+            <a style={{ margin: 'auto', marginRight: '0', fontSize: '2vw'}} href="https://github.com/therealmtam/mysite">See this site's React code</a>
+          </div>
+        </nav>
+
+        {this.renderView(this.state.currentView[0])}
       </div>
-    )
+    );
   }
 }
+
+const HomePage = ({changeView}) => (
+  <div className="homepage">
+    <ul className="list">
+      <li className="list-titleblock" >
+        <span className="list-title">Latest Projects</span>
+      </li>
+      <li className="list-link">
+        <button className="list-button">
+          <span className="list-link">
+            <a className="list-link" href="http://ec2-54-241-149-218.us-west-1.compute.amazonaws.com/">Image Hosting Microservice</a>
+          </span>
+        </button>
+      </li>
+      <p className="list-link-description">Part of an app built using
+        Service Oriented Architecture - independently served and stored 40kb images
+        &lt;<a href="https://github.com/therealmtam/airbnbclone">code</a>&gt;
+      </p>
+      <li className="list-link">
+        <button className="list-button">
+          <span className="list-link">
+            <a className="list-link" href="https://youtu.be/iBNh73Uqp8E">Packuno</a>
+          </span>
+        </button>
+      </li>
+      <p className="list-link-description">An app that helps you pack by providing you
+        with relevant travel info and storing your packing lists
+        &lt;<a href="https://github.com/Gem-Guys/packuno">code</a>&gt;
+      </p>
+      <li className="list-link">
+        <button className="list-button">
+          <span className="list-link">
+              <a className="list-link" href="https://maxtam.herokuapp.com/">Gem Slack</a>
+          </span>
+        </button>
+      </li>
+      <p className="list-link-description">A model of Slack - a real-time team collaboration tool
+        &lt;<a href="https://github.com/therealmtam/Gem-Slack/tree/herokudeployed">code</a>&gt;
+      </p>
+    </ul>
+
+    <ul className="list">
+      <li className="list-titleblock">
+        <span className="list-title">Startups</span>
+      </li>
+      <li className="list-link">
+        <button className="list-button">
+          <span className="list-link">
+            <a className="list-link" href="https://youtu.be/XkNBvIEHqBs">Notos</a>
+          </span>
+        </button>
+      </li>
+      <p className="list-link-description">Building Next Gen Car Security</p>
+      <li className="list-link">
+        <button className="list-button">
+          <span className="list-link">
+            <a className="list-link" href="https://youtu.be/BCdyemecCjU">Cook Ally</a>
+          </span>
+        </button>
+      </li>
+      <p className="list-link-description">Making Your Stove Intelligent</p>
+    </ul>
+  </div>
+);
+
+const ImageHost = () => {};
+const Packuno = () => {};
+const GemSlack = () => (
+  <div className="gemslack">
+    <span className="gemslack-title">Gem Slack</span>
+    <p className="gemslack-text">
+      <div style={{margin: '2vw 0vw'}}>
+        <a href="https://maxtam.herokuapp.com/" style={{fontSize: '3vw'}}>Open Gem Slack</a>
+      </div>
+      <ul className="gemslack-list">
+        <li>Messages disappear after 10 minutes : )</li>
+        <li><a href="https://github.com/therealmtam/Gem-Slack/tree/herokudeployed">Checkout the repo</a></li>
+      </ul>
+
+    </p>
+  </div>
+);
+
 
 App.propTypes = {
 };
 
 export default App;
-
-
-
-
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const webpack = require('webpack');
+
 const path = require('path');
+
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
@@ -6,7 +11,7 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     loaders: [
@@ -15,9 +20,20 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(), // minify everything
+    new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
 };
